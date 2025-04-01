2a4dc493a26fe5158ff4eddea80c785000afc75c
Abdallah-55 <abdoram999@gmail.com>
2025-04-01 12:14:21 +0200

    day 1 - commit 1

---
+++ template.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    freopen("C:/Entertainment/Study/CS/CP/input.txt", "r", stdin);
+    freopen("C:/Entertainment/Study/CS/CP/output.txt", "w", stdout);
+
+    // write code here
+}
